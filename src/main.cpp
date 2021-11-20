228800ddab0db9dba0a6e632b7b75e8419b3b78f
daniel3108-pl <swietlikd@gmail.com>
2021-11-20 16:39:27 +0100

    Dodanie bibliotek dla urzadzen

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
+// Kod Zrodlowy - Projektu SMiW
+
+// Autor: Daniel Swietlik
+// Wydzial: Politechnika Slaska AEiI
+// Kierunek: Informatyka Katowice
+// Temat: Projekt urzadzenia kontrolujacego wentylator za pomoca podczerwieni
+
+// Importowanie Bibliotek Arduino
 #include <Arduino.h>
+#include <DFRobot_SHT3x.h>
+#include <LiquidCrystal_I2C.h>
 
 void setup() {
   // put your setup code here, to run once:
