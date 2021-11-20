0b181b8b879d4a1fe70890b3ec06749fb79a814b
daniel3108-pl <swietlikd@gmail.com>
2021-11-20 17:59:19 +0100

    Zmiany w zmiennych

--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,41 @@
 #include <LiquidCrystal_I2C.h>
 #include <IRremote.h>
 
-// Kody podczerwieni pilota do wentylatora | rc = remote control
-#define rcOn    0x00
-#define rcOff   0x00
-#define rcMode1 0x00
-#define rcMode2 0x00
-#define rcMode3 0x00
+// Definiowanie kodow podczerwieni pilota do wentylatora | rc = remote control
+#define rcOnOffLen 1
+unsigned int rcOnOffRawData[rcOnOffLen] = {
+  0
+};
+
+#define rcChngModeLen 1
+unsigned int rcChngModeRawData[rcChngModeLen] = {
+  0
+};
 
 // Definiowanie Pinow modulow i przyciskow itp
-#define irTransPIN  1
-#define irRecvPIN   1
 #define UP          1
-#define DOWN        1
-#define SET         1
+#define DOWN        2
+#define SET         3
+#define irTransPIN  4
+#define irRecvPIN   5
+
+// Adresy I2C modulow lcd 2x16 i czujnika temperatury sht31-f
+#define LCD_I2C     0x27
+#define SHT31F_I2C  0x44
 
 // Definiowanie obiektow modulow i przyciskow, zmiennych
-float curTemp   = 0;
-float mode1Temp = 0;
-float mode2Temp = 0;
-float mode3Temp = 0;
+unsigned int curFanMode = 0; // curFanMode = 0  => Wentylator jest wylaczony
+float curTemp = 0.0f;
 
+float mode1Temp = 0.0f; // curFanMode = 1
+float mode2Temp = 0.0f; // curFanMode = 2
+float mode3Temp = 0.0f; // curFanMode = 3
+
+
+// Do zdobycia kodow pilota ir
 IRrecv irrecv(irRecvPIN);
 decode_results results;
-
+// IRsend irSend(irTransPIN);
 
 // Funkcje wykonujace dzialanie urzadzenia
 
