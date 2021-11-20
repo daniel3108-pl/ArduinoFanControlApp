2c1363cdf969f051f4787f0085caabb495273e52
daniel3108-pl <swietlikd@gmail.com>
2021-11-20 16:48:15 +0100

    Dodanie paru wstepnych zmienny do pozniejszej zmiany

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,16 @@
 #include <Arduino.h>
 #include <DFRobot_SHT3x.h>
 #include <LiquidCrystal_I2C.h>
+#include <IRremote.h>
+
+// Definiowanie Piny modulow i przyciskow
+const int irTransPIN = 1;
+const int irRecvPIN = 1;
+const int UP = 1;
+const int DOWN = 1;
+const int SET = 1;
+
+// Definiowanie obiektow modulow i przyciskow
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,4 +26,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void irRAWSetup(){
+  
 }
