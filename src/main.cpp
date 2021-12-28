3f1bd00a657de48dd8ff017a7aa88ef49c1afa2f
daniel3108-pl <swietlikd@gmail.com>
2021-12-28 14:13:49 +0100

    Dodanie implementacji funkcji przerwan, wyswietlania ui i controlowania wiatraka

--- src/main.cpp
+++ src/main.cpp
@@ -6,31 +6,29 @@
 // Temat: Projekt urzadzenia kontrolujacego wentylator za pomoca podczerwieni
 
 // Importowanie Bibliotek Arduino
+
 #include <Arduino.h>
 #include <DFRobot_SHT3x.h>
 #include <LiquidCrystal_I2C.h>
 #include <IRremote.h>
-#include <math.h>
+#include <string.h>
+
 
 // Definiowanie kodow podczerwieni pilota do wentylatora | rc = remote control
-#define rcOnOffLen 1
-unsigned int rcOnOffRawData[rcOnOffLen] = {
-  0
-};
 
-#define rcChngModeLen 1
-unsigned int rcChngModeRawData[rcChngModeLen] = {
-  0
-};
+#define RC_ONOFF      0xFF807F
+#define RC_NEXTMODE   0xFF807F
+#define RC_REPEAT     0xFFFFFF
 
 // Definiowanie Pinow modulow i przyciskow itp
-#define UP            0x1
-#define DOWN          0x2
-#define SET           0x3
-#define IR_TRANS_PIN  0x4
-#define IR_RECV_PIN   0x5
+
+#define UP            0x0
+#define DOWN          0x1
+#define SET           0x2
+#define IR_TRANS_PIN  0x3
 
 // Adresy I2C modulow lcd 2x16 i czujnika temperatury sht31-f
+
 #define SHT31F_I2C  0x45
 #define LCD_I2C     0x27
 #define LCD_COLS    16
@@ -38,111 +36,162 @@ unsigned int rcChngModeRawData[rcChngModeLen] = {
 
 // Definiowanie obiektow modulow i przyciskow, zmiennych
 
-unsigned int curUiPage = 0; // 0 => pokazuje aktualna temperature
+volatile unsigned int curUiPage = 0; // 0 => pokazuje aktualna temperature
 unsigned int curFanMode = 0; // curFanMode = 0  => Wentylator jest wylaczony
-float curTemp = 0.0f;
-float mode1Temp = 0.0f; // curFanMode = 1
-float mode2Temp = 0.0f; // curFanMode = 2
-float mode3Temp = 0.0f; // curFanMode = 3
 
+float curTemp = 0.0f; // Aktualna temperatura
+volatile float mode1Temp = 0.0f; // curFanMode = 1
+volatile float mode2Temp = 0.0f; // curFanMode = 2
+volatile float mode3Temp = 0.0f; // curFanMode = 3
+bool fanON = false;
 
-IRrecv irrecv(IR_RECV_PIN);
-decode_results results;
 
 IRsend irSend(IR_TRANS_PIN);
 LiquidCrystal_I2C display(LCD_I2C, LCD_COLS, LCD_ROWS);
 DFRobot_SHT3x temperatureSensor;
 
 // Funkcje wykonujace dzialanie urzadzenia
+
 float roundFPrec(float, int);
-void irRAWDataScraping();
-bool isButtonClicked(unsigned int);
-void userInterfaceLogic();
+// bool isButtonClicked(unsigned int pin);
+void setButtonHandler();
+void upButtonHandler();
+void downButtonHandler();
 void displayCurUIPage();
 void fanControl();
 
 // Implementacja funkcji urzadzenia
 
 void setup() {
+
+  pinMode(SET, INPUT_PULLUP);
+  pinMode(UP, INPUT_PULLUP);
+  pinMode(DOWN, INPUT_PULLUP);
+  
+  attachInterrupt(digitalPinToInterrupt(UP), upButtonHandler, FALLING);
+  attachInterrupt(digitalPinToInterrupt(DOWN), downButtonHandler, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SET), setButtonHandler, FALLING);
+
+  display.init();
+  display.print("Starting...");
+  display.backlight();
   Serial.begin(9600);
 
-  pinMode(UP, INPUT);
-  pinMode(DOWN, INPUT);
-  pinMode(SET, INPUT);
-
-  display.begin(LCD_COLS, LCD_ROWS, LCD_5x8DOTS);
-  temperatureSensor.begin();
-  irSend.begin(IR_TRANS_PIN, false, 0);
-  irrecv.begin(IR_RECV_PIN);
 }
   
-void loop() {
+void loop(){
+  fanControl();
+  displayCurUIPage();
+  delay(200);
+}
+
+// Button Handlery - Implementacja
+
+// bool isButtonClicked(unsigned int pin) {
+//   if (digitalRead(pin) == LOW) {
+//     delay(20);
+//     if (digitalRead(pin) == LOW)
+//       return true;
+//   }
+//   return false;
+// }
+
+void setButtonHandler(){
+  curUiPage++;
+  curUiPage %= 4;
+}
+void upButtonHandler(){
+  switch (curUiPage)
+  {
+  case 1:
+    mode1Temp += 0.5f;
+    break;
+  case 2:
+    mode2Temp += 0.5f;
+    break;
+  case 3:
+    mode3Temp += 0.5f;
+    break;
+  default:
+    break;
+  }
+}
+void downButtonHandler(){
+  switch (curUiPage)
+  {
+  case 1:
+    mode1Temp -= 0.5f;
+    break;
+  case 2:
+    mode2Temp -= 0.5f;
+    break;
+  case 3:
+    mode3Temp -= 0.5f;
+    break;
+  default:
+    break;
+  }
+}
+
+void displayCurUIPage() {
   curTemp = temperatureSensor.getTemperatureC();
-
-}
-
-void irRAWDataScraping(){
-
-}
-
-bool isButtonClicked(unsigned int pin){
-  if (digitalRead(pin) == HIGH) {
-    delay(20);
-    if (digitalRead(pin) == HIGH)
-      return true;
-  }
-  return false;
-}
-
-void userInterfaceLogic(){
-  if (isButtonClicked(UP)) { 
-
-  } 
-  else if (isButtonClicked(DOWN)) {
-
-  }
-  else if (isButtonClicked(SET)) { 
-
-  }
-  else {
-
-  }
-}
-
-void displayCurUIPage(){
-  switch (curUiPage){
+  switch (curUiPage) {
     case 0:
+      display.clear();
+      display.print("Current Temp");
+      display.setCursor(0,1);
+      display.print(String(curTemp) + " *C");
       break;
     case 1:
+      display.clear();
+      display.print("Set - Mode I");
+      display.setCursor(0,1);
+      display.print(String(mode1Temp) + " *C");
       break;
     case 2:
+      display.clear();
+      display.print("Set - Mode II");
+      display.setCursor(0,1);
+      display.print(String(mode2Temp) + " *C");
       break;
     case 3:
+      display.clear();
+      display.print("Set - Mode III");
+      display.setCursor(0,1);
+      display.print(String(mode3Temp) + " *C");
       break;
     default:
       break;
   }
 }
 
-void fanControl(){
-
+void fanControl() {
   float ctemp = roundFPrec(curTemp, 1);
 
-  if( ctemp >= mode1Temp && ctemp < mode2Temp ) { 
-    
+  if (ctemp >= mode1Temp && ctemp < mode2Temp && !fanON) { 
+      irSend.sendNEC(RC_ONOFF, 32);
+      delay(50);
+      irSend.sendNEC(RC_NEXTMODE, 32);
+      delay(50);
+      irSend.sendNEC(RC_NEXTMODE, 32);
+      fanON = true;
   }
-  else if( ctemp >= mode2Temp && ctemp < mode3Temp ) {
-
+  else if (ctemp >= mode2Temp && ctemp < mode3Temp) {
+      irSend.sendNEC(RC_NEXTMODE, 32);
+      delay(50);
   }
-  else if( ctemp >= mode3Temp ) { 
-
+  else if (ctemp >= mode3Temp) { 
+      irSend.sendNEC(RC_NEXTMODE, 32);
+      delay(50);
   }
-  else { 
-
+  else if (fanON){ 
+      irSend.sendNEC(RC_ONOFF, 32);
+      delay(50);
+      fanON = false;
   }
 }
 
-float roundFPrec(float value, int prec){
+float roundFPrec(float value, int prec) {
   int multipl = pow(10, prec);
   return (float)((int)(value * (float)multipl + 0.5)) / (float)multipl; 
 }
